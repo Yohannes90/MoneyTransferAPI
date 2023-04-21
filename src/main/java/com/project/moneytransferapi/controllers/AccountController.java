b15fa8239aa070fa47b39ecbe5253f6e10afdd12
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-21 16:50:38 +0300

    account controller

---
+++ src/main/java/com/project/moneytransferapi/controllers/AccountController.java
@@ -0,0 +1,23 @@
+package com.project.moneytransferapi.controllers;
+
+import com.project.moneytransferapi.domains.Account;
+import com.project.moneytransferapi.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/api/accounts")
+    public Account registerNewAccount(@RequestBody Account newAccount) {
+        return accountService.createAccount(newAccount);
+    }
+}
